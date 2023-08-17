18e2b73a2820328bc59ab9b7d974d6766740fa95
kamwysoc <hello@kamwysoc.com>
2023-08-17 20:35:34 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -16,43 +16,29 @@ let argonCTarget: Target = .target(name: "phc-winner-argon2",
                                     "src/blake2/blamka-round-opt.h"
                                    ],
                                    sources: [
-                                    "phc-winner-argon2/src/argon2.c'",
-                                    "phc-winner-argon2/src/core.{c,h}'",
-                                    "phc-winner-argon2/src/thread.{c,h}'",
-                                    "phc-winner-argon2/src/encoding.{c,h}'",
-                                    "phc-winner-argon2/src/blake2/blake2.h'",
-                                    "phc-winner-argon2/src/blake2/blake2b.c'",
-                                    "phc-winner-argon2/src/blake2/blake2-imp.h",
-                                    "phc-winner-argon2/include/**/*.h",
-                                    "phc-winner-argon2/src/ref.c",
-                                    "phc-winner-argon2/src/blake2/blamka-round-ref.h"
+                                    "src"
+//                                    "phc-winner-argon2/src/argon2.c'",
+//                                    "phc-winner-argon2/src/core.{c,h}'",
+//                                    "phc-winner-argon2/src/thread.{c,h}'",
+//                                    "phc-winner-argon2/src/encoding.{c,h}'",
+//                                    "phc-winner-argon2/src/blake2/blake2.h'",
+//                                    "phc-winner-argon2/src/blake2/blake2b.c'",
+//                                    "phc-winner-argon2/src/blake2/blake2-imp.h",
+//                                    "phc-winner-argon2/include/**/*.h",
+//                                    "phc-winner-argon2/src/ref.c",
+//                                    "phc-winner-argon2/src/blake2/blamka-round-ref.h"
                                    ],
                                    publicHeadersPath: "include"
 )
 
 let mainTarget: Target = .target(name: "SignalArgon2",
                                  dependencies: ["phc-winner-argon2"],
-                                 path: ".",
-                                 exclude: [
-                                    "android",
-                                    "gradle",
-                                    "Gemfile",
-                                    "Gemfile.lock",
-                                    "gradlew",
-                                    "gradlew.bat",
-                                    "LICENSE",
-                                    "settings.gradle",
-                                    "SignalArgon2.podspec",
-                                 ],
-                                 sources: [
-                                    "ios/src"
-                                 ],
+                                 exclude: [],
                                  linkerSettings: [.linkedLibrary("phc-winner-argon2",
                                     .when(platforms: [.iOS]))])
 
 let testTarget: Target = .testTarget(name: "SignalArgon2Tests",
                                      dependencies: ["SignalArgon2"])
-testTarget.path = "ios/tests"
 
 
 let package = Package(
